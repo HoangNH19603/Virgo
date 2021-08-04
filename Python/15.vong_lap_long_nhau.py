5a39f82c9edd8a9221db41949eb9f44fa75b0ca6
Huy Hoang Nguyen <19t6n2003@gmail.com>
2021-08-04 22:10:46 +0700

    Delete

--- Python/15.vong_lap_long_nhau.py
+++
@@ -1,8 +0,0 @@
-n = int(input("nhap vao so n: "))
-for y in range (n):
-    for x in range (n):
-        if x == 0 or x == y or y == n-1: #x == n-1:
-            print ("*", end='')
-        else:
-            print (" ", end='')
-    print ()
--- Python/6.1.If_Else_nang_cao.py
+++
@@ -1,7 +0,0 @@
-a = int(input("nhap a: "))
-b = int(input("nhap b: "))
-c = 10 if a + b >= 10 else 5
-print ("c = " + str(c))
-
-x = int(input("nhap 1 so: "))
-print ("la so chan" if x % 2 == 0 else "la so le")
