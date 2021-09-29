c4ea1fde6473bb6a9764a4973a4d98dc731c5492
Huy Hoang Nguyen <hoangnhph22961@fpt.edu.vn>
2021-09-29 14:51:24 +0700

    Update

--- Python/21.eval.py
+++ Python/21.eval.py
@@ -1,20 +1,18 @@
 '''
-Hàm Eval là 1 hàm mạnh mẽ có thể thực hiện được các chuỗi phép toán, hàm có thể xác định 1 list được truyền trong 1 chuỗi và các lệnh đơn giản
-Ham` Eval() chuyen cac Parameter thanh cau lenh, neu dung cu phap cau lenh se duoc thuc thi
+Eval() function convert parameter inside it to code
 more: https://www.w3schools.com/python/ref_func_eval.asp
 '''
 
 from time import *
-s = input("code: ")
-x = eval(s) #có thể đẩy vào 1 lệnh như print ("hello world") để hàm xuất ra kết quả trên màn hình
-print (x)
+# eval(input("enter a command ")) #I can put the command print("hello world") to Eval()
 
-sum = eval(input("Code: ")) #hàm eval có thể thực hiện các phép tính được nhập vào từ dạng chuỗi
-print (sum)
+# sum = eval(input("Code: ")) #hàm eval có thể thực hiện các phép tính được nhập vào từ dạng chuỗi
+# print (sum)
 
 sleep(1)
 list_str = "[1,2,3,4,5]"
-l1st = eval(list_str) #chuyển chuỗi list_str sang list
+l1st = eval(list_str) #convert from string to list
 print (l1st)
-sleep(1)
-print (l1st [2])
+for i in l1st:
+    print (i)
+    sleep(1)
