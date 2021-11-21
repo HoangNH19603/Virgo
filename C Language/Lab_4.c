b53b546f38718bf0e444ef9209c01b1a01f779e5
Pino Nguyễn <42845390+HoangNguyen1906@users.noreply.github.com>
2021-11-21 18:06:09 +0700

    Update Lab 4

--- C Language/Lab_4.c
+++ C Language/Lab_4.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int luachon, item, min, max, count, snt_scp, temp;	//global variable
+long tong;
+
+void snt(int snt_scp){	//ham kiem tra so nguyen to
+	if(snt_scp<2){
+        printf("\n%d khong phai SNT",snt_scp);
+    }
+    else{
+        for(item=2; item<snt_scp; item++){
+            if(snt_scp % item == 0){
+                printf("\n%d khong phai SNT",snt_scp);
+                return;
+            }
+        }
+        printf("\n%d la SNT", snt_scp);
+        return;
+    }
+}
+
+void scp(int snt_scp){	//ham kiem tra so chinh phuong
+	for(item=1; item<=snt_scp; item++){
+		if(item*item == snt_scp){
+		printf("%d la so chinh phuong",snt_scp);
+		return;
+		}
+	}
+	printf("%d khong phai so chinh phuong", snt_scp);
+}
 
 void main(){
-	int luachon, item, min, max, tong, count, snt_scp;
 	do{
 		item=0, tong=0, count=0;	//reset gia tri
+		system("cls");	//clear man hinh windows
 		printf("\nChuc nang 1: Tinh trung binh tong");
 		printf("\nChuc nang 2: Tim so nguyen to");
 		printf("\nChuc nang 3: Tim so chinh phuong");
-		printf("\nChuc nang 4: Thoat");
+		printf("\nChuc nang 4: Thoat\n");
 		printf("\nXin moi chon chuc nang (1,2,3,4): ");
 		scanf("%d", &luachon);
 		switch(luachon)
@@ -15,6 +45,19 @@ void main(){
 			case 1:
 				printf("\nNhap vao min, max: ");
 				scanf("%d %d", &min, &max);
+				if(min%2!=0 || max%2!=0){   //Cach 1: tinh theo cong thuc
+					if(min%2!=0)    min++;
+					else if(max%2!=0)    max--;
+				}
+				tong=min/2+max/2;
+				printf("Trung binh tong = %ld", tong);
+				
+				// if(min>max){ //Dao gia tri neu min>max (chi dung trong cach 2,3)
+				//     temp=max;
+				//     max=min;
+				//     min=temp;
+				// }
+				/*  Cach 2: vong lap for
 				for(min; min<=max; min++)
 				{
 					if(min%2==0)
@@ -22,8 +65,9 @@ void main(){
 					tong += min;
 					count++;
 					}
-				}
-				/*	Cach 2
+				}*/
+				
+				/*	Cach 3: vong lap while
 				item = min;
 				while(item <= max)
 				{
@@ -34,29 +78,27 @@ void main(){
 					}
 					item++;
 				}*/
-				printf("\nTrung binh tong = %d", tong/count);
+				// if(count==0)	count = 1;
+				// printf("\nTrung binh tong = %ld", tong/count);    //Dung trong cach 2, cach 3
 				break;
 			case 2:
 				printf("\nNhap vao 1 so: ");
 				scanf("%d", &snt_scp);
-				for(item=2; item<snt_scp; item++){
-					if(snt_scp % item == 0)	count++;
-				}
-				if(count==0 && snt_scp!=1)	printf("%d la so nguyen to", snt_scp);
-				else	printf("%d khong phai so nguyen to", snt_scp);
+				snt(snt_scp);
 				break;
 			case 3:
 				printf("\nNhap vao 1 so: ");
 				scanf("%d", &snt_scp);
-				for(item=1; item<=snt_scp; item++){
-					if(item*item == snt_scp){
-					printf("%d la so chinh phuong",snt_scp);
-					count++;
-					}
-				}
-				if(count==0)	printf("%d khong phai so chinh phuong", snt_scp);
+                scp(snt_scp);
 				break;
+			case 4:
+				printf("\nKet thuc chuong trinh");
+				break;
+			default:
+				printf("\nChi nhap gia tri trong khoang tu 1 den 4");
 		}
+		system("pause");	//tam dung
 	}	while(luachon != 4);
 	printf(":U bai baiiiiii");
+	return 0;
 }
