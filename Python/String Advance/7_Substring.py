e83e222671546b2b204ed5b75c3d8b4b28474c67
Huy Hoang Nguyen <19t6n2003@gmail.com>
2021-11-21 18:22:54 +0700

    Update python

--- Python/String Advance/7_Substring.py
+++ Python/String Advance/7_Substring.py
@@ -5,4 +5,8 @@ string[start:end:step]    get all characters from start to end -1 discounting ev
 string[start:end]   get all characters from start to end -1
 string[start:]      get all character from index start to the end of string
 string[end:]    get all characters from the beginning of the string to end -1
+string[:]    get all characters from the beginning of the string to the end of the string
+string[::-1]    get all characters in reverse order
 '''
+string = "Reverse string"
+print(string[::-1])
