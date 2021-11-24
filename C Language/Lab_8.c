de3845efaa81c71aa2f8e2037f1a9921724fd26b
Nguyen Huy Hoang <19t6n2003@gmail.com>
2021-11-24 21:54:24 +0700

    Update Lab 8 C

--- C Language/Lab_8.c
+++ C Language/Lab_8.c
@@ -11,16 +11,19 @@ void main() {
 	int soluong, dem;
 	printf("Nhap vao so luong sinh vien muon them: ");
 	scanf("%d", &soluong);
-	struct sinhvien sv[5];
+	struct sinhvien sv[100];
 	for (dem  = 0; dem < soluong; dem++)
 	{
 		printf("Nhap vao mssv: ");
 		scanf("%s", mssv);
 		strcpy(sv[dem].mssv, mssv);
+		scanf("%s", ten);
+		strcpy(sv[dem].tensv, ten);
+		scanf("%s", mssv);
+		strcpy(sv[dem].nganh, nganh);
 	}
 	for (dem = 0; dem < soluong; dem++)
 	{
 		printf("%s\n", sv[dem].mssv);
 	}
-	getchar();
 }
