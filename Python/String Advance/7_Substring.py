a4d31effeda3986b46ea1a6566f6003518fbd1f3
Nguyen Huy Hoang <19t6n2003@gmail.com>
2021-12-05 18:25:20 +0700

    Update exam

--- Python/String Advance/7_Substring.py
+++ Python/String Advance/7_Substring.py
@@ -4,10 +4,11 @@ Example
 string[start:end:step]    get all characters from start to end -1 discounting every step character
 string[start:end]   get all characters from start to end -1
 string[start:]      get all character from index start to the end of string
-string[end:]    get all characters from the beginning of the string to end -1
+string[:end]    get all characters from the beginning of the string to end -1
 string[:]    get all characters from the beginning of the string to the end of the string
 string[::-1]    get all characters in reverse order
 '''
 # string = "Reverse string"
 # print(string[::-1])
 print("LeveL"[::-1])
+print("LeveL"[1:-1])
