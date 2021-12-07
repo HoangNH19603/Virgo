4326fe35fc3504bf328ca28648d50185747f1d9c
Nguyen Huy Hoang <42845390+HoangNH19603@users.noreply.github.com>
2021-12-07 19:12:26 +0000

    nothing

--- Python/String Advance/2_ljust_cjust_rjust.py
+++ Python/String Advance/2_ljust_cjust_rjust.py
@@ -3,9 +3,9 @@
 # rjust == right justify
 # syntax: object.method(width,fillchar)
 
-name = input('Name ')
-age = int(input('Age '))
-DoB = input('Date of Birth ')
+name = input('Name: ')
+age = int(input('Age: '))
+DoB = input('Date of Birth: ')
 print(name.center(30))
 print("Date of Birth ".ljust(15),DoB.rjust(15))
 print("Age ".ljust(15),str(age).rjust(15))
