8f76b3541025c4dc3ac8b791a701471cab280248
Nguyen Huy Hoang <19t6n2003@gmail.com>
2023-10-24 18:06:12 +0700

    update Csharp

--- Csharp/get_device_info/Program.cs
+++ Csharp/get_device_info/Program.cs
@@ -1,3 +1,111 @@
-﻿using System;
-using System.Runtime.InteropServices;
+﻿using 
 
+class Program
+{
+    static readonly IHardwareInfo hardwareInfo = new HardwareInfo();
+
+    static void Main(string[] _)
+    {
+        //hardwareInfo.RefreshOperatingSystem();
+        //hardwareInfo.RefreshMemoryStatus();
+        //hardwareInfo.RefreshBatteryList();
+        //hardwareInfo.RefreshBIOSList();
+        //hardwareInfo.RefreshCPUList();
+        //hardwareInfo.RefreshDriveList();
+        //hardwareInfo.RefreshKeyboardList();
+        //hardwareInfo.RefreshMemoryList();
+        //hardwareInfo.RefreshMonitorList();
+        //hardwareInfo.RefreshMotherboardList();
+        //hardwareInfo.RefreshMouseList();
+        //hardwareInfo.RefreshNetworkAdapterList();
+        //hardwareInfo.RefreshPrinterList();
+        //hardwareInfo.RefreshSoundDeviceList();
+        //hardwareInfo.RefreshVideoControllerList();
+
+        hardwareInfo.RefreshAll();
+
+        Console.WriteLine(hardwareInfo.OperatingSystem);
+
+        Console.WriteLine(hardwareInfo.MemoryStatus);
+
+        foreach (var hardware in hardwareInfo.BatteryList)
+            Console.WriteLine(hardware);
+
+        foreach (var hardware in hardwareInfo.BiosList)
+            Console.WriteLine(hardware);
+
+        foreach (var cpu in hardwareInfo.CpuList)
+        {
+            Console.WriteLine(cpu);
+
+            foreach (var cpuCore in cpu.CpuCoreList)
+                Console.WriteLine(cpuCore);
+        }
+
+        Console.ReadLine();
+
+        foreach (var drive in hardwareInfo.DriveList)
+        {
+            Console.WriteLine(drive);
+
+            foreach (var partition in drive.PartitionList)
+            {
+                Console.WriteLine(partition);
+
+                foreach (var volume in partition.VolumeList)
+                    Console.WriteLine(volume);
+            }
+        }
+
+        Console.ReadLine();
+
+        foreach (var hardware in hardwareInfo.KeyboardList)
+            Console.WriteLine(hardware);
+
+        foreach (var hardware in hardwareInfo.MemoryList)
+            Console.WriteLine(hardware);
+
+        foreach (var hardware in hardwareInfo.MonitorList)
+            Console.WriteLine(hardware);
+
+        foreach (var hardware in hardwareInfo.MotherboardList)
+            Console.WriteLine(hardware);
+
+        foreach (var hardware in hardwareInfo.MouseList)
+            Console.WriteLine(hardware);
+
+        foreach (var hardware in hardwareInfo.NetworkAdapterList)
+            Console.WriteLine(hardware);
+
+        foreach (var hardware in hardwareInfo.PrinterList)
+            Console.WriteLine(hardware);
+
+        foreach (var hardware in hardwareInfo.SoundDeviceList)
+            Console.WriteLine(hardware);
+
+        foreach (var hardware in hardwareInfo.VideoControllerList)
+            Console.WriteLine(hardware);
+
+        Console.ReadLine();
+
+        foreach (var address in HardwareInfo.GetLocalIPv4Addresses(NetworkInterfaceType.Ethernet, OperationalStatus.Up))
+            Console.WriteLine(address);
+
+        Console.WriteLine();
+
+        foreach (var address in HardwareInfo.GetLocalIPv4Addresses(NetworkInterfaceType.Wireless80211))
+            Console.WriteLine(address);
+
+        Console.WriteLine();
+
+        foreach (var address in HardwareInfo.GetLocalIPv4Addresses(OperationalStatus.Up))
+            Console.WriteLine(address);
+
+        Console.WriteLine();
+
+        foreach (var address in HardwareInfo.GetLocalIPv4Addresses())
+            Console.WriteLine(address);
+
+        Console.ReadLine();
+    }
+}
