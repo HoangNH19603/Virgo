fb1d156ddf40fc75815879a868c50b0a374b302f
Nguyen Huy Hoang <19t6n2003@gmail.com>
2023-11-05 18:10:23 +0700

    update

---
+++ Csharp/http_request/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+HttpClient client = new HttpClient();
+HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://dummyjson.com/quotes/random");
+HttpResponseMessage response = await client.SendAsync(request);
+string data = await response.Content.ReadAsStringAsync();
+Console.WriteLine($"Status: {response.StatusCode}");
+Console.WriteLine($"{data}");
+client.Dispose();
